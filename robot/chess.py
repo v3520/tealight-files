fdc1b03d583042aedfa2a94fa18387598dc932b4
v3520 <cdavies.31687@farnborough.ac.uk>
2014-08-19 15:12:47 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -9,24 +9,23 @@ from tealight.robot import (move,
 # Add your code here
 
 for n in range(0, 4):
-  
 
-for n in range(0,32):
+  for n in range(0,32):
   move()
   
-turn(1)
+  turn(1)
 
-for n in range(0, 4):
+  for n in range(0, 4):
   move()
   
-turn(1)
+  turn(1)
 
-for n in range(0,32):
+  for n in range(0,32):
   move()
   
-turn(3)
+  turn(3)
 
-for n in range(0,4):
+  for n in range(0,4):
   move()
   
-turn(2)
+  turn(2)
