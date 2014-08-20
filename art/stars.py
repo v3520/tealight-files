7cdb035cefb98b6e6b1424a5e3d40818da81c308
v3520 <cdavies.31687@farnborough.ac.uk>
2014-08-20 09:48:11 +0100

    Running art/stars.py

--- art/stars.py
+++ art/stars.py
@@ -2,20 +2,19 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 from math import sin, cos, pi
 
-def star(x, y, c, size, spines):
+def star(x, y, c, xsize, ysize, spines):
   
   color(c)
+  
   
   angle = 0
   
   for i in range(0, spines):
-    x0 = x + (size * cos(angle))
-    y0 = y + (size * sin(angle))
+    x0 = x + (xsize * cos(angle))
+    y0 = y + (ysize * sin(angle))
     
     line(x, y, x0, y0)
     
     angle = angle + (2 * pi / spines)
 
-star(300, 300, "blue", 100, 50)
-star(600, 400, "purple", 200, 100)
-star(450, 200, "orange", 125, 30)
+star(300, 300, "blue", 200, 100, 50)
