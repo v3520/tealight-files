e787c3fef0caa081e8bef1cb73aa50644eb4bc14
v3520 <cdavies.31687@farnborough.ac.uk>
2014-08-21 12:07:35 +0100

    Running art/minesweeper_bombtest.py

--- art/minesweeper_bombtest.py
+++ art/minesweeper_bombtest.py
@@ -78,7 +78,7 @@ def BombCheck(x,y):
   
   for (i,j) in [(x-1,y-1),(x-1,y), (x-1, y+1), (x,y-1), (x, y+1), (x+1,y-1),(x+1,y), (x+1, y+1)]:
   
-    if (i > 0 and i < WLimit and j > 0 and j < HLimit):
+    if (i >= 0 and i < WLimit and j >= 0 and j < HLimit):
       if BombArray[i][j] == -1:
         BombCount += 1  
   
