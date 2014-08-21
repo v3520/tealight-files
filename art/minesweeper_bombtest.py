5e5b33d4ba7176d202decdf90e882d99855d9e6d
v3520 <cdavies.31687@farnborough.ac.uk>
2014-08-21 11:35:18 +0100

    Running art/minesweeper_bombtest.py

--- art/minesweeper_bombtest.py
+++ art/minesweeper_bombtest.py
@@ -8,5 +8,5 @@ def BombCheck():
   
 for (i,j) in [(x-1,y-1),(x-1,y), (x-1, y+1), (x,y-1), (x, y+1), (x+1,y-1),(x+1,y), (x+1, y+1)]:
   
-  if (i >=0 and i <= WLimit and j>=0 and j<=HLimit
-  
+  if (i > 0 and i < WLimit and j > 0 and j < HLimit
+      
