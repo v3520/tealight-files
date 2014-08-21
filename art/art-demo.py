a410405c0b680a8a051b97dac7fefbb207d5e6b1
v3520 <cdavies.31687@farnborough.ac.uk>
2014-08-21 11:01:39 +0100

    Closing art/art-demo.py

--- art/art-demo.py
+++ art/art-demo.py
@@ -1,39 +1,7 @@
+
+
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 from tealight.art import (screen_width, screen_height)
 
 print "This is art mode!"
-
-print screen_width
-print screen_height
-
-background("track.png")
-
-line(0,0,screen_width, screen_height)
-
-spot(200,300, 20)
-
-circle(300,200, 20)
-
-box(500, 500, 60, 60)
-
-image(200,200,"bird.png")
-
-line(560,0,560,495)
-
-text(600, 100, "Hello Tealight!")
-
-lastx = None
-lasty = None
-hue = 0
-
-def handle_mousemove(x,y):
-  global lastx, lasty, hue
-  
-  line(lastx or x, lasty or y, x, y)
-  color("hsl(%d,100%%,50%%)" % hue)
-  
-  hue += 1
-  
-  lastx = x
-  lasty = y
