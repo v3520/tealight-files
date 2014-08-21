a17242db34b255f73e5cbac68e4ae87ae04f07d3
v3520 <cdavies.31687@farnborough.ac.uk>
2014-08-21 11:28:30 +0100

    Running art/minesweeper_bombtest.py

--- art/minesweeper_bombtest.py
+++ art/minesweeper_bombtest.py
@@ -3,10 +3,11 @@ import github.davidsamueljones.art.Minesweeper
 from random import random, randint
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
+def BombCheck():
+
 CurrentLocation[x][y]
 
 for (i,j) in [(x-1,y-1),(x-1,y), (x-1, y+1), (x,y-1), (x, y+1), (x+1,y-1),(x+1,y), (x+1, y+1)]:
   
-  if (i >=0 and i <= WLimit and j>=0 and j<=HLimit
+  if (i >=0 and i <= WLimit and j>=0 and j<=HLimit:
   test(i,j)
-  
