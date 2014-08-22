0a1b3fd57e962019dd508c1de3159e645f149022
v3520 <cdavies.31687@farnborough.ac.uk>
2014-08-22 10:49:31 +0100

    Running art/minesweeper_bombtest.py

--- art/minesweeper_bombtest.py
+++ art/minesweeper_bombtest.py
@@ -169,7 +169,7 @@ def FloodBoard(x,y):
           FloodBoard(i,j)
       
 NumberOfBombs = 15
-HLimit = 35
+HLimit = 34
 WLimit = HLimit
 SquareSize = 500/HLimit
 StartingX = screen_width /2 - SquareSize * WLimit/2
