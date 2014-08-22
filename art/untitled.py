8863bec56ebe3dc23cb8700e4c6dcd619b1838d5
v3520 <cdavies.31687@farnborough.ac.uk>
2014-08-22 09:58:09 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -169,7 +169,7 @@ def FloodBoard(x,y):
         if BombArray[i][j] == 0:
           FloodBoard(i,j)
       
-NumberOfBombs = 50
+NumberOfBombs = 1
 HLimit = 20
 WLimit = HLimit
 SquareSize = 500/HLimit
